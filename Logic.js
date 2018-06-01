1c4a4a616623564ef96ac0c9562bbb794da05c1f
James Daniel Semilla <jedsemilla@gmail.com>
2018-05-31 20:40:31 -0400

    Initial Commit

---
+++ Logic.js
@@ -0,0 +1,90 @@
+// The logic file for the site
+
+// The list of planets
+var PlanetList = [
+    {
+        Name_Offical: "Earth", 
+        NickName: "A big spinning marble", 
+        Age: 50000, 
+        Image: "Images/IMG_Earth.jpg",
+        Description: "The place where all humans live. It has been around for billions of years even more." + 
+                     "So please take of it...pretty please."
+    },
+    {
+        Name_Offical: "Venus", 
+        NickName: "One of the first planets in warframe", 
+        Age: 8000, 
+        Image: "Images/IMG_Venus.jpg",
+        Description: "Aside from being one of the first planets in Warframe it is also...actually I don't know."
+    }
+];
+
+
+// Page references
+var buttonSpawnPoint;
+var planetDisplaySpawnPoint;
+
+// When the page loads up
+window.onload = function()
+{
+    buttonSpawnPoint = document.querySelector("#PlanetSelectionSpawnPoint");
+    planetDisplaySpawnPoint = document.querySelector(".PlanetInfoSpawnPoint");
+
+    // Create planet selection buttons
+    createPlanetSelectionButtons();
+};
+
+// Create planet selection buttons
+function createPlanetSelectionButtons()
+{
+    buttonSpawnPoint.innerHTML = "";
+
+    // For each entry in the planet list
+    for (var i = 0; i < PlanetList.length; i++)
+    {
+        var newButton;
+        newButton = document.createElement("Button");
+        newButton.appendChild(document.createTextNode(PlanetList[i].Name_Offical));
+        newButton.setAttribute("class", "btn btn-default");
+        newButton.setAttribute("onclick", "displayPlanet(" + i + ")");
+
+        buttonSpawnPoint.appendChild(newButton);
+    }
+}
+
+// Display planet
+function displayPlanet(index)
+{
+    planetDisplaySpawnPoint.innerHTML = " ";
+
+    // The planet name title
+    var planetNameTitle = document.createElement("h3");
+    planetNameTitle.appendChild(document.createTextNode(PlanetList[index].Name_Offical));
+
+    // The nick name title
+    var nickNameTitle = document.createElement("h4");
+    nickNameTitle.appendChild(document.createTextNode("Nickname: " + PlanetList[index].NickName));
+
+    // The age of the planet text
+    var ageText = document.createElement("h4");
+    ageText.appendChild(document.createTextNode("Age: " + PlanetList[index].Age));
+
+    // Create the image elements
+    var planetImage = document.createElement("img");
+    planetImage.setAttribute("src", PlanetList[index].Image);
+
+    // Create the description elements
+    var descriptionTitle = document.createElement("h4");
+    descriptionTitle.appendChild(document.createTextNode("Details"))
+
+    var description = document.createElement("p");
+    description.appendChild(document.createTextNode(PlanetList[index].Description));
+
+    // Add the new elements to the designated spawnpoint
+    planetDisplaySpawnPoint.appendChild(planetNameTitle);
+    planetDisplaySpawnPoint.appendChild(nickNameTitle);
+    planetDisplaySpawnPoint.appendChild(ageText);
+    planetDisplaySpawnPoint.appendChild(planetImage);
+    planetDisplaySpawnPoint.appendChild(descriptionTitle);
+    planetDisplaySpawnPoint.appendChild(description);
+}
