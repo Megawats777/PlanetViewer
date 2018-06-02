26f2510deb74c47055c74661c48aa823ecb06853
Megawats777 <jedsemilla@gmail.com>
2018-06-02 14:56:21 -0400

    Added planet info toggle to certain properties depending if the user is in the alternate dimension or not

--- Logic.js
+++ Logic.js
@@ -1,5 +1,8 @@
 // The logic file for the site
 
+/*--Variables--*/
+
+
 // The list of planets
 var PlanetList = [
     {
@@ -10,7 +13,11 @@ var PlanetList = [
         CarbonDangerLevel: "Ludicrous",
         Image: "Images/IMG_Earth.jpg",
         Description: "The place where all humans live. It has been around for billions of years even more." + 
-                     " So please take of it...pretty please."
+                     " So please take of it...pretty please.",
+        Alt_Dim_NickName: "A pristine paradise",
+        Alt_Dim_Age: 20,
+        Alt_Dim_Population: "20 Thousand",
+        Alt_Dim_CarbonDangerLevel: "Light"
     },
     {
         Name_Offical: "Venus", 
@@ -19,7 +26,11 @@ var PlanetList = [
         Population: "200 Million",
         CarbonDangerLevel: "Light",
         Image: "Images/IMG_Venus.jpg",
-        Description: "Aside from being one of the first planets in Warframe it is also...actually I don't know."
+        Description: "Aside from being one of the first planets in Warframe it is also...actually I don't know.",
+        Alt_Dim_NickName: "The yellow devil",
+        Alt_Dim_Age: 9500,
+        Alt_Dim_Population: "400",
+        Alt_Dim_CarbonDangerLevel: "Moderate"
     }
 ];
 
@@ -27,12 +38,27 @@ var PlanetList = [
 // Page references
 var buttonSpawnPoint;
 var planetDisplaySpawnPoint;
+var alternateDimensionToggle;
+
+var isInAlternateDimension = false;
+
+
+/*--Variables End--*/
+
 
 // When the page loads up
 window.onload = function()
 {
     buttonSpawnPoint = document.querySelector("#PlanetSelectionSpawnPoint");
     planetDisplaySpawnPoint = document.querySelector("#PlanetInfoSpawnPoint");
+    alternateDimensionToggle = document.querySelector("#AltDimensionToggle");
+
+    alternateDimensionToggle.onchange = function()
+    {
+        isInAlternateDimension = alternateDimensionToggle.checked;
+        toggleAlternateDimension(alternateDimensionToggle.checked);
+    }
+
 
     // Create planet selection buttons
     createPlanetSelectionButtons();
@@ -63,17 +89,50 @@ function displayPlanet(index)
 
     // The planet name title
     var planetNameTitle = document.createElement("h4");
-    planetNameTitle.appendChild(document.createTextNode(PlanetList[index].Name_Offical + " (" + PlanetList[index].NickName + ")"));
+    var nickNameContent = " ";
+
+    if (isInAlternateDimension)
+    {
+        nickNameContent = PlanetList[index].Alt_Dim_NickName;
+    }
+    else
+    {
+        nickNameContent = PlanetList[index].NickName;
+    }
+
+    planetNameTitle.appendChild(document.createTextNode(PlanetList[index].Name_Offical + " (" + nickNameContent + ")"));
     planetDisplaySpawnPoint.appendChild(planetNameTitle);
 
     // The age of the planet text
     var ageText = document.createElement("p");
-    ageText.appendChild(document.createTextNode("Age: " + PlanetList[index].Age));
+    var ageContent = " ";
+
+    if (isInAlternateDimension)
+    {
+        ageContent = PlanetList[index].Alt_Dim_Age;
+    }
+    else
+    {
+        ageContent = PlanetList[index].Age;
+    }
+
+    ageText.appendChild(document.createTextNode("Age: " + ageContent));
     planetDisplaySpawnPoint.appendChild(ageText);
 
     // The population text
     var populationText = document.createElement("p");
-    populationText.appendChild(document.createTextNode("Population: " + PlanetList[index].Population));
+    var populationContent = " "
+
+    if (isInAlternateDimension)
+    {
+        populationContent = PlanetList[index].Alt_Dim_Population;
+    }
+    else
+    {
+        populationContent = PlanetList[index].Population;
+    }
+
+    populationText.appendChild(document.createTextNode("Population: " + populationContent));
     planetDisplaySpawnPoint.appendChild(populationText);
 
     // Carbon danger level text
@@ -81,7 +140,18 @@ function displayPlanet(index)
     carbonDangerLevelText_01.appendChild(document.createTextNode("CO Danger Level: "));
 
     var carbonDangerLevelText_02 = document.createElement("p");
-    carbonDangerLevelText_02.appendChild(document.createTextNode(PlanetList[index].CarbonDangerLevel));
+    var carbonDangerLevelContent = " ";
+
+    if (isInAlternateDimension)
+    {
+        carbonDangerLevelContent = PlanetList[index].Alt_Dim_CarbonDangerLevel;
+    }
+    else
+    {
+        carbonDangerLevelContent = PlanetList[index].CarbonDangerLevel;
+    }
+
+    carbonDangerLevelText_02.appendChild(document.createTextNode(carbonDangerLevelContent));
     carbonDangerLevelText_02.style.display = "inline";
     
     // Depending of the content of the second carbon danger level text element
@@ -122,4 +192,22 @@ function displayPlanet(index)
     description.appendChild(document.createTextNode(PlanetList[index].Description));
     planetDisplaySpawnPoint.appendChild(description);
     
+}
+
+// Toggle the status of the alternate dimension
+function toggleAlternateDimension(status)
+{
+    var displayedPlanetTitle = document.querySelector("#DisplayedPlanetTitle");
+
+    if (status === true)
+    {
+        displayedPlanetTitle.innerHTML = "Selected Planet (Alternate Dimension)";
+    }
+
+    else
+    {
+        displayedPlanetTitle.innerHTML = "Selected Planet";
+    }
+
+    planetDisplaySpawnPoint.innerHTML = "<p>No planet selected.</p>";
 }
